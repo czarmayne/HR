bb51959f2d4f598779d76d9d32985e1cd1e61f95
czarmayne <mczarmayne@gmail.com>
2022-02-26 15:15:23 +0800

    Added initial dependencies

--- app/src/main/java/com/b2b/employee/monitoring/App.java
+++ app/src/main/java/com/b2b/employee/monitoring/App.java
@@ -3,12 +3,17 @@
  */
 package com.b2b.employee.monitoring;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+        System.out.println(new App().getGreeting());
     }
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+    public String getGreeting() {
+        return "Hello World!";
     }
 }
