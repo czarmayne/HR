59c529062e6be13ea4f2eda48b0bee5172823828
czarmayne <mczarmayne@gmail.com>
2022-02-26 19:30:32 +0800

    MongoDB: Update for Entities

--- app/src/main/java/com/b2b/employee/monitoring/core/entity/Management.java
+++ app/src/main/java/com/b2b/employee/monitoring/core/entity/Management.java
@@ -3,12 +3,16 @@ package com.b2b.employee.monitoring.core.entity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Builder
 @Getter
 @Setter
+@Document("management")
 public class Management {
 
+    @Id
     public long id;
     public long managerId;
     public long directReportId;
